8057390ad04489efb14ec3ea48511bb29ec83599
Aryan Mehta <aryannmehta@gmail.com>
2023-10-23 17:07:39 +0530

    ✨ Feat: Add sign-in and sign-out using

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
 const DashboardPage = () => {
   return (
     <div>
       Dashboard Page
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 };
